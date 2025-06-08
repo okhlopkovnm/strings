8d0fd3bb405d16a39c7de69a09a5ba15a7f2f0ab
okhlopkov_nm <okhlopkov_nm@mail.ru>
2025-06-09 08:34:32 +0900

    clang

--- strnccmp.c
+++ strnccmp.c
@@ -5,9 +5,9 @@
 
 #include "s21_string.h"
 
-int s21_strncmp(const char *str1, const char *str2, s21_size_t n){
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
   int ret = 0;
-  for (int i = 0; i < n; ++i){
+  for (int i = 0; i < n; ++i) {
     ret = str1[i] - str2[i];
     if (ret != 0) {
       break;
